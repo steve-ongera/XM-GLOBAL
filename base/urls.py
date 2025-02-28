ac6bff60a1827c0bed26aa593ae88cceabc671c5
steve-ongera <gadafisteve001@gmail.com>
2025-02-28 23:22:33 +0300

    added new urls for my base app i.e dashboard url

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard , name='dashboard'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
